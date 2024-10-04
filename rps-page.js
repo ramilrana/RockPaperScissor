c29fee1a554bc3c9dbe502db762a2d6796eb5bfc
Ramil Rana <ramilrana20@gmail.com>
2024-10-04 09:41:17 +0800

    Chore: Added sweet alert

---
+++ rps-page.js
@@ -0,0 +1,9 @@
+let backButton = document.getElementById("back");
+
+backButton.onclick = function () {
+  location.replace("../index.html");
+};
+
+function swalClick() {
+  swalMessage("1", "2", "success", "Play Again");
+}
