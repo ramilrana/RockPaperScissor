393744aefba9170b290eac5d5636fec3898c5de5
Ramil <ramilrana20@gmail.com>
2024-10-07 07:18:13 +0800

    Chore: added html and css styling

--- rps-page.js
+++ rps-page.js
@@ -1,9 +1,9 @@
-let backButton = document.getElementById("back");
+let backButton = document.getElementById("back")
 
 backButton.onclick = function () {
-  location.replace("../index.html");
-};
+  location.replace("../index.html")
+}
 
 function swalClick() {
-  swalMessage("1", "2", "success", "Play Again");
+  swalMessage("1", "2", "success", "Play Again")
 }
