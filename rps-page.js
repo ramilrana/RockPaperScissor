e23dbe915cae48b8418cb15c67f61b6ed62cf1b0
Ramil Rana <ramilrana20@gmail.com>
2024-10-18 15:23:23 +0800

    Chore: Completed the UI needed

--- rps-page.js
+++ rps-page.js
@@ -1,9 +1,74 @@
-let backButton = document.getElementById("back")
+window.onload = function () {
+  disableButton(".toDisable", true);
+  getRounds();
+  toStart();
+  hideResetButton(true);
+  reloadPage();
+};
 
-backButton.onclick = function () {
-  location.replace("../index.html")
+const startGameBTN = document.getElementById("startGame");
+const roundSelect = document.getElementById("rounds");
+const resetBTN = document.getElementById("resetGame");
+
+let rockPick = document.getElementById("rockPick");
+let paperPick = document.getElementById("paperPick");
+let scissorPick = document.getElementById("scissorPick");
+
+function getRounds() {
+  roundSelect.onchange = function () {
+    console.log(roundSelect.value);
+    document.querySelector(".startBTN").disabled = false;
+    document.getElementById("rounds").style.width = "4rem";
+  };
 }
 
-function swalClick() {
-  swalMessage("1", "2", "success", "Play Again")
+function toStart() {
+  startGameBTN.onclick = function () {
+    Swal.fire({
+      title: "STARTING GAME",
+      text: "Starting Rock, Paper, and Scissor, click Confirm button to continue.",
+      icon: "info",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Confirm",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        disableButton(".toDisable", false); // Enable buttons
+        startGameBTN.hidden = true;
+        roundSelect.disabled = true;
+        hideResetButton(false);
+      }
+    });
+  };
 }
+
+function disableButton(buttonClass, toDisable) {
+  const buttons = document.querySelectorAll(buttonClass);
+  buttons.forEach((button) => {
+    button.disabled = toDisable;
+  });
+}
+function hideResetButton(toHide) {
+  resetBTN.hidden = toHide; // Hides the button
+}
+
+function reloadPage() {
+  resetBTN.onclick = function () {
+    Swal.fire({
+      title: "RESTART GAME?",
+      text: "If you restart game, there will be no winner for the match. Click Restart button to continue.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Confirm",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        location.reload();
+      }
+    });
+  };
+}
+
+function playerPick(choice) {}
